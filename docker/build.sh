b0540db60dfce7bfacffeab627466e8db22192c9
Lothar Rubusch <l.rubusch@gmx.ch>
2019-11-13 00:58:49 +0100

    build.sh: quickfix for setting initially correct permissions for the altera usb device..

--- docker/build.sh
+++ docker/build.sh
@@ -2,5 +2,8 @@
 ## prepare SDK environment
 source ~/env.sh
 
+## TODO: this is my "poor people's udev" (aaargh - sic transit gloria mundi!)
+sudo chmod 666 /dev/bus/usb/$(lsusb | grep "Altera" | awk '{ print $2 "/" $4 }' | tr -d ':')
+
 ## start quartus
 /opt/altera/quartus/bin/quartus
