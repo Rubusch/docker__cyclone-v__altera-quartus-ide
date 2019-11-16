fd2ab876973842eb337ccc2fcd55fa3e0fcb096c
Lothar Rubusch <l.rubusch@gmx.ch>
2019-11-16 22:25:48 +0100

    build.sh: update, fix bogus device handling

--- docker__quartus-16.1/build.sh
+++ docker__quartus-16.1/build.sh
@@ -1,9 +1,7 @@
 #!/bin/bash -e
+
 ## prepare SDK environment
 source ~/env.sh
 
-## TODO: this is my "poor people's udev" (aaargh - sic transit gloria mundi!)
-sudo chmod 666 /dev/bus/usb/$(lsusb | grep "Altera" | awk '{ print $2 "/" $4 }' | tr -d ':')
-
 ## start quartus
-/opt/altera/quartus/bin/quartus
+quartus
